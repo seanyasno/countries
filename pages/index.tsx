33a1c779dc5bcdbfb9da13d549fb92dcc15347c4
Sean Yasnogrodski <sean.yasno@gmail.com>
2022-07-12 01:24:54 +0300

    Added input.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,8 @@ import {fetchAllCountries} from '../src/requests';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import {useQuery} from 'react-query';
-import {useEffect} from 'react';
-import {Box, Grid, Typography} from '@mui/material';
+import {useEffect, useMemo, useState} from 'react';
+import {Box, Grid, Input, InputBase, Typography} from '@mui/material';
 import {CountryCard} from '../src/components';
 
 const Home: NextPage = () => {
@@ -13,6 +13,10 @@ const Home: NextPage = () => {
         queryKey: 'countries',
         queryFn: fetchAllCountries,
     });
+
+    const [input, setInput] = useState<string>();
+
+    const filteredCountries = useMemo(() => countries?.filter(country => country.name.common.includes(input ?? '')), [countries, input]);
 
     // useEffect(() => {
     //     console.log(countries);
@@ -32,10 +36,32 @@ const Home: NextPage = () => {
 
             <main className={styles.main}>
                 <Box sx={{padding: '0px 30px'}}>
+                    <InputBase
+                        placeholder='Search for a country...'
+                        onChange={event => setInput(event.target.value)}
+                        sx={theme => ({
+                            margin: '0 0 50px 0',
+                            maxWidth: '400px',
+                            '& .MuiInputBase-input': {
+                                borderRadius: 2,
+                                position: 'relative',
+                                backgroundColor: theme.palette.mode === 'light' ? 'hsl(0, 0%, 100%)' : '#2b2b2b',
+                                border: '0px none #ced4da',
+                                fontSize: 14,
+                                padding: '16px 24px',
+                                transition: theme.transitions.create([
+                                    'border-color',
+                                    'background-color',
+                                    'box-shadow',
+                                ]),
+                                boxShadow: theme.shadows[3]
+                            },
+                        })}
+                    />
                     <Grid container spacing={10}>
                         {
                             isSuccess &&
-                            countries.map((country, index) => (
+                            filteredCountries?.map((country, index) => (
                                 <Grid key={index} item xs={12} sm={6} md={4} lg={3}>
                                     <CountryCard country={country}/>
                                 </Grid>
